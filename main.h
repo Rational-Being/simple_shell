c3c6062974d879c16f1b2cb284b89d3c7867f240
Jesse Okoro <okorojesse@gmail.com>
2023-09-14 22:18:31 -0700

    making the code a compact one

--- main.h
+++ main.h
@@ -8,5 +8,10 @@
 #include <sys/wait.h>
 extern char **environ;
 extern void free (void *__ptr) __THROW;
+void gui();
+int execute_if_abs_path(const char *command, char *const args[]);
+int execute_search_path(const char *command, char *const args[]);
+int launch_command(const char *command_line);
+
 
 #endif
--- ok_yu_shell.c
+++ ok_yu_shell.c
@@ -1,33 +1,11 @@
 #include "main.h"
 
-
-/*
- * _getenv - function to get an environment variable
- * @name: name of the command
- * Return: returns null
- */
-
-char *_getenv(const char *cmd_name)
-{
-	int i = 0;
-	size_t name_len = strlen(name);
-
-	while (environ[i] != NULL)
-	{
-		if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
-			return environ[i] +name_len +1;
-		i++;
-	}
-	return (NULL);
-}
-
-
-/*
+/**
  * gui - function to display shell prompt
  * Return: Nothing
  */
 
-void gui()
+void gui(void)
 {
 	char prompt[] = "Okoro_Yusuff_Shell# ";
 
@@ -35,68 +13,10 @@ void gui()
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 }
 
-/*
- * launch_command - command that execute command
- * @commandline: commadn passed
- * Return: 0 on sucess
+/**
+ * main - the main funtion for th simple shell
+ * Return: 0 on success
  */
-
-int launch_command(char *commandline)
-{
-	char *token;
-	/* duplicate the command line*/
-	char *temp = strdup(commandline);
-	int arg_count = 0, status;
-	char *args[4];
-	pid_t child_pid;
-
-	if (temp == NULL)
-	{
-		perror("Failed to allocate memory");
-		return (1);
-	}
-
-	token = strtok(temp, " ");
-	while(token != NULL)
-	{
-		args[arg_count++] = token;
-		token = strtok(NULL, " ");
-	}
-	/*null terminate the args array*/
-	args[arg_count] = NULL;
-
-	if (arg_count > 0)
-	{
-		if (strchr(args[0], '/') != NULL)
-			{
-				child_pid = fork();
-		
-				if (child_pid == -1)
-			{
-				perror("No such file or directory");
-				free(temp);
-				exit(EXIT_FAILURE);
-			}
-			else if (child_pid == 0)
-			{	
-				if (execve(args[0], args, environ) == -1)
-				{
-					perror("Failed to Execute Command");
-					free(temp);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				wait(&status);
-				free(temp);
-			}
-			}
-	}
-	else
-	
-	return (0);
-}
 
 int main(void)
 {
@@ -117,30 +37,30 @@ int main(void)
 			if (feof(stdin))
 				break;
 
-			else 
+			else
 			{
 				perror("Okoro_Yusuff Shell:  Command Entered Doesn't Exist");
 				/* print error then clear the buffer i.e. tmep */
-				while ((clear_temp = getchar()) != '\n' && clear_temp != EOF);
-				free(temp);
-				/* Iniatialize temp and temp_size to defualt so that the program can continue to run normally*/
+				while ((clear_temp = getchar()) != '\n' && clear_temp != EOF)
+					free(temp);
+				/* Iniatialize temp and temp_size to defualt so that*/
+				/*the program can continue to run normally*/
 				temp = NULL;
 				temp_size = 0;
 				/*Go back to reading input*/
-				continue; 
+				continue;
 
 			}
 		}
-		
 		else if (temp[cmd - 1] == '\n')
-				temp[cmd - 1] = '\0';
+			temp[cmd - 1] = '\0';
 	
-	launch_command(temp);
-	
+		launch_command(temp);
+	}
+
 	free(temp);
 	temp = NULL;
 	temp_size = 0;
-	}
 
 	return (0);
 }
